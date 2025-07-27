fdc55ae46d8e25574d6cad0ff6098514a8a546b0
aaaaaaaa <rih28184@gmail.com>
2025-07-27 12:20:34 +0900

    add

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-event-app/',
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:5050',
         changeOrigin: true,
         secure: false,
       }
